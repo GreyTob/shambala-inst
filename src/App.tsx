7650af91fc94f1f30ddc21645096749bc00df0d8
Sergey <GreyTob@inbox.ru>
2021-11-12 17:38:31 +0500

    update eract-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import Loader from './containers/Loader/Loader';
 import Main from './containers/Main/Main';
@@ -8,6 +8,7 @@ import Course from './containers/Course/Course';
 import Layout from './hoc/layout/Layout';
 
 import { Context } from './Context';
+
 
 const App: React.FC = () => {
   const [isLoad, setIsLoad] = useState<boolean>(false);
@@ -19,23 +20,20 @@ const App: React.FC = () => {
 
   const data = useContext(Context);
   
-
   return (
     <>
       {!isLoad && !sessionStorage.getItem('isFirstLoad') ? (
         <Loader />
       ) : (
         <Layout>
-          <Switch>
-            <Route path="/" exact component={Main} />
+          <Routes>
+            <Route path="/" element={<Main />} />
 
             {data!.courses.map((course) => (
-              <Route path={course.to} component={Course} key={course.to} />
+              <Route path={course.to} element={<Course />} key={course.to} />
             ))}
 
-            <Redirect to="/"/>
-
-          </Switch>
+          </Routes>
         </Layout>
       )}
     </>
